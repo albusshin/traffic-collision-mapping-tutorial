f789bb9f15350a52172b895ed5e85e8d39012996
Albus Shin <AlbusShin@gmail.com>
2015-03-15 18:08:55 +0800

    Added CartoDB SQL files

---
+++ cartoDBAlterTable.sql
@@ -0,0 +1,11 @@
+ALTER TABLE untitled_table DROP COLUMN name;
+ALTER TABLE untitled_table DROP COLUMN description;
+
+ALTER TABLE untitled_table ADD persons_killed int;
+ALTER TABLE untitled_table ADD persons_injured int;
+ALTER TABLE untitled_table ADD pedestrians_killed int;
+ALTER TABLE untitled_table ADD pedestrians_injured int;
+ALTER TABLE untitled_table ADD cyclists_killed int;
+ALTER TABLE untitled_table ADD cyclists_injured int;
+ALTER TABLE untitled_table ADD motorists_killed int;
+ALTER TABLE untitled_table ADD motorists_injured int;
